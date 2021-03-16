1919b6371d70be9dbaaebb5afa3e2beeaae288c6
krlos07 <48899362+krlos07@users.noreply.github.com>
2021-03-16 09:50:52 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
 	<meta charset="utf-8">
-	<title></title>
-	<link rel="stylesheet" href="">
-	<script src="js/jquery-3.6.0.slim.min.js"></script>
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<title>Prueba Técnica</title>
+	<link rel="stylesheet" href="css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 	<script src="js/funciones.js"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </head>
 <body>
-	<div class="container">
-		<h2>SENDERISMO</h2>
+	<div class="main container">
+		<h2>PRUEBA TÉCNICA DESARROLLO 1</h2>
 		<div class="container">
-			<form action="" method="post">
+			<img class="mountain" src="img/mountain.png">
+			<form method="POST" action="modulo.php">
 				<div class="form-group">
 					<table class="table table-bordered table-hover">
 						<tr>
@@ -24,24 +24,22 @@
 					</table>
 				</div>
 				<div class="form-group">
-					<div id="btnAgregar" class="btn btn-success">AGREGAR FILA</div>
 					<table id="tablaElementos" class="table table-bordered table-hover">
 						<tr>
 							<th>Peso</th>
 							<th>Calorías</th>
-							<th>Opciones</th>
+							<th></th>
 						</tr>
 						<tr>
 							<td><input class="form-control" type="text" name="pesoElemento[]" placeholder="Peso del elemento"></td>
 							<td><input class="form-control" type="text" name="caloriaElemento[]" placeholder="Propiedad calórica"></td>
 							<td class="text-center">
-								<div class="btn btn-danger" type="button" name="eliminar">ELIMINAR</div>
+								<div id="btnAgregar" class="btn btn-success">AÑADIR FILA</div>
 							</td>
 						</tr>	
 					</table>
-					<div class="btn btn-success">CALCULAR</div>
+					<input class="btn btn-primary" type="submit" name="calcular" value="CALCULAR" >
 				</div>
-				<input class="form-control" type="text" name="salida" value="" placeholder="salida">
 			</form>
 		</div>
 	</div>
--- modulo.php
+++ modulo.php
@@ -1,34 +1,37 @@
 <?php
-	function calcular(){
-		$calorias = 25;//$_POST['calorias'];
-		$peso = 10;//$_POST['peso'];
+	//RECUPERO LAS VARIABLES POR POST
+		$peso = $_POST['peso'];
+		$calorias = $_POST['calorias'];
+		$pesoElemento = $_POST['pesoElemento'];
+		$caloriaElemento = $_POST['caloriaElemento'];
 		$pesoFinal = 0;
 		$caloriaFinal = 0;
 		$solucion="";
-		$elementos = [
-			[6,8],
-			[1,7],
-			[1,4],
-			[3,3],
-			[5,2]
-		];
-		
+		$elementos = array();
+	//LLENO UN ARREGLOCON LOS DATOS OBTENIDOS Y LES ASIGNO UN INDICE
+		for ($i=0; $i < sizeof($pesoElemento); $i++) { 
+			$elementos[] = array($i+1, $pesoElemento[$i], $caloriaElemento[$i]);
+		}
+	//ORDENO DE MANERA DESCENDENTE CON RESPECTO A LAS CALORIAS 	
+		array_multisort(array_column($elementos, 2), SORT_DESC, $elementos);
+	//CREO UN CICLO PARA QUE RECORRA EL ARREGLO Y COMPARE LAS VARIABLES
+	//Y A SU VES VOY ACTUALIZANDO LAS VARIABLES	
 		$i=0;
 		while ($peso>0){
-			if ($peso>=$elementos[$i][0]) {
-				$peso=$peso-$elementos[$i][0];
-				$caloriaFinal=$caloriaFinal+$elementos[$i][1];
-				$pesoFinal=$pesoFinal+$elementos[$i][0];
-				$solucion=$solucion."Peso: ".$elementos[$i][0]." Calorias: ".$elementos[$i][1]."<br>";
+			if ($peso>=$elementos[$i][1]) {
+				$peso=$peso-$elementos[$i][1];
+				$caloriaFinal=$caloriaFinal+$elementos[$i][2];
+				$pesoFinal=$pesoFinal+$elementos[$i][1];
+				$solucion=$solucion."Elemento (".$elementos[$i][0]."): Peso: ".$elementos[$i][1]." Calorias: ".$elementos[$i][2].'<br>';
 			}else{
+	//CUANDO YA NO SE PUEDA AGREGAR OTRO ELEMENTO POR LA CONDICION DEL PESO
+	//LE DAMOS EL VALOR DE 0 PARA QUE CULMINE EL CICLO
 				$peso=0;
 			}
 			$i++;
 			//break;
 		}
-		echo $solucion;
-		echo "Peso total: ".$pesoFinal."<br>Caloria total: ".$caloriaFinal;
-
-	}
-	calcular();
+	//SALIDA SOLUCIÓN CON SUS TOTALES
+		$especific = 'Peso total: '.$pesoFinal.' Caloria total: '.$caloriaFinal;
+		echo "<body style='background-image: url(img/fondo.jpg);'><div style='width:72%; heiht: 10vh; margin: 0 auto; text-align: center; font-family: sans-serif; margin-top: 100px; background-color: #FEFEFE; padding: 20px; border-radius: 10px; box-shadow: 2px 2px 2px 2px;'><h1 style='margin: 50 auto;'>ELEMENTOS OPTIMOS</h1><p style='text-align: center; margin: 100 auto; font-size: 20px;'>$solucion$especific</p><br><a href='index.php'>VOLVER</a></body></div>";
 ?>
